94f20fe2c83c0b38c20fa3eb91831f7d40934bec
George Evans <george.r.evans@googlemail.com>
2017-07-13 00:13:47 +0100

    5.10 added admin authorization to delete comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,6 +30,14 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
     
     user  ||=User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+        can :manage, :all
+    else
+        can :read, :all
+        can :manage, User, id: user.id
+        can :manage, Comment, user_id: user.id
+        cannot [:create, :update, :destroy], Product
+    end
+
   end
 end
